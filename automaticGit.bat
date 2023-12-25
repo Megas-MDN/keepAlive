04db14b132ffeed773f593c3ce5fbd7a88123053
megas-mdn <mdn.melqui@gmail.com>
2023-12-25 19:06:18 -0300

    N:

---
+++ automaticGit.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+
+if "%1"=="" (
+    echo Fail!
+    exit /b 1
+)
+
+set "mensagemCommit=%1"
+
+git add .
+git commit -m "%mensagemCommit%"
+git push
+
+echo Sucess!
+exit /b 0
---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require('fs/promises');
+const { exec } = require('child_process');
+
+const caminhoBatch = 'automaticGit.bat';
+
+const mensagemCommit = 'Adding new file automatically';
+
+async function criarArquivo() {
+  const arquivoNovo = `file-${Date.now()}.txt`;
+
+  await fs.writeFile(
+    `./files/${arquivoNovo}`,
+    `${mensagemCommit} - ${new Date()} \n`,
+    { encoding: 'utf8' }
+  );
+}
+
+criarArquivo().then(() => {
+  const callCommand = `${caminhoBatch} N: ${new Date()}`;
+  // return console.log(`Executando o arquivo batch: ${callCommand}`);
+  exec(`call ${callCommand}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Erro ao executar o arquivo batch: ${error.message}`);
+      return;
+    }
+    console.log(`Saída do processo:\n${stdout}`);
+  });
+});
